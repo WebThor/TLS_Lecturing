52c0fb4b3755ea0ca4b331d4bbe8576e039aaa75
WebThor <thorsten.weber@rwth-aachen.de>
2025-11-22 10:52:46 +0100

    client added

--- secure-client.py
+++ secure-client.py
@@ -1,15 +1,11 @@
 import socket
 import ssl
 
-HOST = "127.0.0.1"   # oder "localhost"
+HOST = "127.0.0.1"
 PORT = 5000
 
-# TLS-Client-Kontext mit Zertifikatsprüfung
-context = ssl.create_default_context(ssl.Purpose.SERVER_AUTH)
-context.load_verify_locations("server.crt")   # Server-Zertifikat als vertrauenswürdig laden
-
-SERVER_NAME = "localhost"  # muss zum CN/SAN im Zertifikat passen
+context = ssl._create_unverified_context()  # keine Zertifikatsprüfung
 
 with socket.create_connection((HOST, PORT)) as sock:
-    with context.wrap_socket(sock, server_hostname=SERVER_NAME) as tls_sock:
+    with context.wrap_socket(sock, server_hostname=HOST) as tls_sock:
         tls_sock.sendall(b"username=alice&password=SuperSecret123")
