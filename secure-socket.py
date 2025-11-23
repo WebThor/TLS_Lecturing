73e963c4f056a244cd3d4babdb43fa2b108b3d9e
WebThor <thorsten.weber@rwth-aachen.de>
2025-11-23 10:38:31 +0100

    client added

--- secure-socket.py
+++ secure-socket.py
@@ -1,11 +1,11 @@
 import socket
 import ssl
 
-HOST = "0.0.0.0"
+HOST = "127.0.0.1"
 PORT = 5000
 
-CERT_FILE = "server.crt"   # Dein Zertifikat
-KEY_FILE = "server.key"    # Dein Private Key
+CERT_FILE = "server.crt"   
+KEY_FILE = "server.key"    
 
 # TLS-Kontext für den Server
 context = ssl.SSLContext(ssl.PROTOCOL_TLS_SERVER)
@@ -13,13 +13,7 @@ context.load_cert_chain(certfile=CERT_FILE, keyfile=KEY_FILE)
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
     sock.bind((HOST, PORT))
-    sock.listen(1)
-    print(f"Listening on {HOST}:{PORT} ...")
-
     conn, addr = sock.accept()
-    print("TCP connection from:", addr)
-
-    # Hier die einzelne Verbindung in TLS wrappen
     with context.wrap_socket(conn, server_side=True) as tls_conn:
         data = tls_conn.recv(1024)
         print("Received:", data.decode("utf-8", errors="replace"))
